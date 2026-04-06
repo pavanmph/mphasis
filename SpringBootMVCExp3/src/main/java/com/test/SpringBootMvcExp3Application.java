8fb80e7b331ff6107a50f86ff7d8d339233d28fe
pavanmph <pavangokule68@gmail.com>
2026-04-06 10:22:35 +0530

    Add files via upload

---
+++ SpringBootMVCExp3/src/main/java/com/test/SpringBootMvcExp3Application.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+
+@SpringBootApplication
+public class SpringBootMvcExp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcExp3Application.class, args);
+	}
+}
+
+
+
